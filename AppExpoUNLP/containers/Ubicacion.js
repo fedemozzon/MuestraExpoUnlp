bc5c0aee2bee4ea7a2c0fadf086141d4ec224aeb
pedroodb <dalbianco.pedro@gmail.com>
2019-09-13 14:05:54 -0300

    Correcciones

--- AppExpoUNLP/containers/Ubicacion.js
+++ AppExpoUNLP/containers/Ubicacion.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react'
 import {
   View,
-  Text,
 } from 'react-native'
 
+// import {
+//   PermissionAware as PermissionAwareComponent,
+//   PermissionConstants,
+// } from 'permission_aware_react_native_component'
+
 class UbicacionContainer extends Component {
+
+  // componentList = [
+  //   ({
+  //     permission:[PermissionConstants.LOCATION],
+  //     connectionRequire:PermissionConstants.WIFI,
+  //     component:(<GPSMap />)
+  //   }),
+  //   ({
+  //     connectionRequire:PermissionConstants.WIFI,
+  //     component:(<ManualMap />)
+  //   }),
+  // ]
 
   render() {
     return (
       <View>
-        <Text>Ubicacion</Text>
+        {/* <PermissionAwareComponent
+          permissionComponentList={componentList}
+          defaultComponent={(<TextInputLoc />)}
+        /> */}
       </View>
     )
   }
