3ecc5cbffdb6c20298b0a369a95c6c472c38f58d
fedediclaudio <federicodicla@gmail.com>
2019-09-16 11:04:58 -0300

    Correciones

--- AppExpoUNLP/containers/Bienvenida.js
+++ AppExpoUNLP/containers/Bienvenida.js
@@ -5,11 +5,7 @@ class BienvenidaContainer extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
       <View style={{flex:1, flexDirection:"column", backgroundColor: "#eceff1"}}>
-=======
-      <View >
->>>>>>> f92b2f72d798d1be967234741f71a25e5b861231
         <Text style={styles.titleText}>Bienvenido</Text>
         <Text style={styles.subtitleText}>Descripcion de lo que hace la aplicacion, ver que poner</Text>
         <View style={styles.comenzarButtonView}>
